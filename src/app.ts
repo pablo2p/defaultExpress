a9c25ccfa1b6cbf6bfd646c528b3486f976bccec
Pablo Paixão <zkyouta@hotmail.com>
2021-12-13 00:13:08 -0300

    Default Express

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { router } from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.use(express.json());
+app.use('/api', router); // Prefix
+
+export { app };
